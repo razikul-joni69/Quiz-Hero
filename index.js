1983a33aa0712410064cc61fd7a40f27eb940251
MD Razikul Islam Joni <mdrazikulislamjoni@gmail.com>
2023-04-14 15:11:24 +0600

    solved results not storing in localstorage

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ const displayQuiz = (data) => {
 
 // EventListener for quiz submit button
 document.querySelector("#submit").addEventListener("click", () => {
-  console.log("clicked")
+  // console.log("clicked")
     if (answers.length < 6) {
         return;
     }
@@ -111,7 +111,9 @@ document.querySelector("#submit").addEventListener("click", () => {
     }
 
     // data setting on local storage and getting data from local storage
-    let storage = JSON.parse(localStorage.getItem("result"));
+    let storage = JSON.parse(localStorage.getItem("results"));
+    // let storage = localStorage.getItem("results");
+    console.log(storage);
     if (storage) {
         localStorage.setItem(
             "results",
